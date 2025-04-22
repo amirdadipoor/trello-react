8c874930ca56832a72f0beb17390db94c86fd6a5
amir dadipoor <amirdadipoor@hotmail.com>
2025-04-22 16:24:53 +0330

    modify add list button

--- src/components/AddListButton.jsx
+++ src/components/AddListButton.jsx
@@ -1,5 +1,13 @@
 import {Button} from "flowbite-react";
 
 export default function AddListButton() {
-    return (<Button className="new-list-button">{'ایجاد لیست جدید +'}</Button>)
+
+    return (/*<Button className="new-list-button">{'ایجاد لیست جدید +'}</Button>*/
+        <div className="fixed bottom-4 right-4 z-50 flex items-center gap-2">
+            <button id="floating-btn"  className="bg-gray-800 hover:bg-gray-700 text-white dark:bg-gray-100 dark:hover:bg-gray-200 dark:text-gray-900 text-xl p-4 rounded-full shadow-lg" >
+                +
+            </button>
+            <span className="bg-gray-700 text-white text-sm px-2 py-1 rounded shadow hidden sm:block">{'ایجاد لیست جدید '}</span>
+        </div>
+    )
 }
--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -2,20 +2,23 @@ import ThemeToggle from "./themes/ThemeToggle.jsx";
 
 export default function AppNavbar() {
     return (
-        <nav className="bg-white fixed top-0 left-0 w-full border-gray-200 stark:bg-gray-900 z-50" id="nav-style">
+        <nav className="bg-gray-100 fixed top-0 left-0 w-full border-gray-200 dark:bg-gray-900 z-50" >
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <ThemeToggle></ThemeToggle>
-                <a href="https://github.com/amirdadipoor/trello-react" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white stark:text-white">Trello react (AMD ver)</span>
-                </a>
+                <div className="flex flex-row">
+                    <ThemeToggle ></ThemeToggle>
+                    <a href="https://github.com/amirdadipoor/trello-react" className="flex items-center space-x-3 rtl:space-x-reverse mr-2">
+                        <span className="self-center mt-2 text-lg font-semibold whitespace-nowrap text-gray-500 dark:text-white">Trello react (AMD ver)</span>
+                    </a>
+                </div>
+
                 <div className="flex md:order-2">
                     <div className="relative hidden md:block">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500 stark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </div>
-                        <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 stark:bg-gray-700 stark:border-gray-600 stark:placeholder-gray-400 stark:text-white stark:focus:ring-blue-500 stark:focus:border-blue-500" placeholder="جستوجو ..."/>
+                        <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-500 placeholder-gray-400 border border-gray-500 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="جستوجو ..."/>
                     </div>
                 </div>
             </div>
